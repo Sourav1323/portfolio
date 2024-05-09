90369d8e59af9fb5e0ff0ae8621a94ccb2286278
Sourav1323 <guptasourav1323@gmail.com>
2024-05-09 10:48:34 +0530

    added code

--- src/component/navbar/NavBar.jsx
+++ src/component/navbar/NavBar.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import "./NavBar.scss"
+import{motion} from "framer-motion"
 const NavBar = () => {
   return (
     <div className='navbar'>
     <div className='wrapper'>
-   <span> <span className='name'>Sourav </span>Gupta</span>
-        <div className='socials'>
-            <a href='#'><img src="../../../public/linkedin.png"></img> </a>
-            <a href='#'><img src="../../../public/github.png"></img> </a>
-            <a href='#'><img src="../../../public/instagram.png"></img> </a>
-        </div>
+   <motion.span  initial={{opacity:0,scale:0.5}} animate={{opacity:1,scale:1}} transition={{duration:0.5}} className='name1'> <span className='name'>Sourav </span>Gupta </motion.span>
+  
+        <motion.div  initial={{opacity:0,scale:0.5}} animate={{opacity:1,scale:1}} transition={{duration:0.5}}className='socials'>
+            <a href='https://www.linkedin.com/in/sourav-gupta-3ab283228/'><img src="../../../public/linkedin.png"></img> </a>
+            <a href='https://github.com/Sourav1323'><img src="../../../public/github.png"></img> </a>
+            <a href='https://www.instagram.com/so.urav502/'><img src="../../../public/instagram.png"></img> </a>
+        </motion.div>
     </div>
-      
+
     </div>
   )
 }
